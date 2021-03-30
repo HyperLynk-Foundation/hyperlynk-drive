df9cb28624bdbff40697e00c1dfc0433241f1e0e
Ali Almazedi <ali.almazedi@boothamstudent.co.uk>
2021-03-30 11:32:59 +0100

    s

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 
 export default function Login() {
     const emailRef = useRef()
@@ -9,6 +9,7 @@ export default function Login() {
     const { login } = useAuth()
     const [error, setError ] = useState('')
     const [loading, setLoading ] = useState(false)
+    const history = useHistory()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -17,6 +18,7 @@ export default function Login() {
             setError('')
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
+            history.push("/")
         } catch {
             setError('failed to sign in')
         }
