d778882ac465ff372b1dedfbb719e6f9bf255e98
Naman P <66672786+HyperLynk@users.noreply.github.com>
2022-06-28 09:04:36 +0100

    Update Navbar.js

--- src/components/desk-drive/Navbar.js
+++ src/components/desk-drive/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import newlifestorelogo from './newlifestorelogo.png'
+import cloud from './cloud .png'
 
 export default function NavbarComponent() {
     return (
         <Navbar bg="light" expand="sm" >
             <Navbar.Brand as={Link} to="/">
-                <img src={newlifestorelogo} width="300" alt="Lifestore" />
+                <img src={cloud } width="300" alt="cloud cf" />
             </Navbar.Brand>
             <Nav>
                 <Nav.Link as={Link} to="/user">
